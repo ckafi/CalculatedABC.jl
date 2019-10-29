a3a4d546363f2eb8163eb225835b744ab8da57d7
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-29 22:05:39 +0000

    build based on 73b21ac

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CalculatedABC.jl-1","page":"CalculatedABC.jl","title":"CalculatedABC.jl","text":"","category":"section"},{"location":"#","page":"CalculatedABC.jl","title":"CalculatedABC.jl","text":"A Julia port of the ABCanalysis R package","category":"page"}]
+}
